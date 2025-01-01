a846b6a68a3d8050a048b3261a879b3ff3fcc882
adrianlavric <115322394+adrianlavric@users.noreply.github.com>
2025-01-01 16:43:50 +0200

    database setup

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE songstorage;
+
+CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,         
+    file_name VARCHAR(255) NOT NULL,
+    artist VARCHAR(255) NOT NULL,  
+    song_name VARCHAR(255) NOT NULL,
+    release_date DATE,              
+    tags TEXT[]                  
+);
+
