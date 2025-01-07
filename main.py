0553807d8116e85cd48671eae9d5b4cd2d24f1ae
adrianlavric <115322394+adrianlavric@users.noreply.github.com>
2025-01-07 16:01:22 +0200

    add pep documentation

--- main.py
+++ main.py
@@ -1,31 +1,73 @@
-import pg8000
-import os
-import shutil
-from datetime import datetime
-import zipfile
-import pygame
-import time
+"""
+SongStorage Application
 
-USER = "postgres"
-PASSWORD = "postgres"
-HOST = "localhost"
-DB = "songstorage"
+This module provides functionality for managing a music library. Users can add,
+delete, modify, search, play songs and create save lists based on criteria.
 
+Features:
+- Add songs to the storage and metadata to the database by path.
+- Delete songs and their metadata by their ID.
+- Modify metadata for existing songs.
+- Search for songs based on criteria.
+- Create a save list archive of songs based on criteria.
+- Play songs using pygame library.
+
+Usage:
+1. Run the script.
+2. Use commands to customize your song library(type 'help' to see the available ones).
+3. Type `quit` to exit the application.
+
+Dependencies:
+- PostgreSQL for the database(psql).
+- Python modules: pg8000, os, shutil, datetime, zipfile, pygame, time
+(use: 'pip install pg8000' and 'pip install pygame' if needed).
+
+Author: Lavric Adrian-Gabriel 3A3, Facultatea de Informatica Iasi, UAIC
+Date: 7.01.2025
+"""
+
+import pg8000 # For connecting to the PostgreSQL database
+import os # For working with files
+import shutil # For copying files
+from datetime import datetime # For handling date formatting
+import zipfile # For creating the archive
+import pygame # For playing songs
+import time # For handling rewinding/forwarding songs
+
+#Database setup
+USER = "postgres" #: Database user name
+PASSWORD = "postgres" #: Database user password
+HOST = "localhost" #: Database host name
+DB = "songstorage" #: Database name
+
+#Valid song extensions
 SONG_EXTENSIONS = {".wav", ".mp3", ".flac", ".ogg", ".aiff", ".mp4"}
 
+#SQL query for table set up
 table_setup_query = """
 CREATE TABLE IF NOT EXISTS songs (
-    id SERIAL PRIMARY KEY,
-    file_name VARCHAR(255) NOT NULL,
-    artist VARCHAR(255) NOT NULL,
-    song_name VARCHAR(255) NOT NULL,
-    release_date DATE,
-    tags TEXT[]
+    id SERIAL PRIMARY KEY,           -- id of each song
+    file_name VARCHAR(255) NOT NULL, -- path of the song
+    artist VARCHAR(255) NOT NULL,    -- name of the artist
+    song_name VARCHAR(255) NOT NULL, -- name of the song
+    release_date DATE,               -- release date of the song
+    tags TEXT[]                      -- list of tags of the song
 );
 """
 
 
 def database_setup():
+    """
+    This method sets up the 'songstorage' PostgreSQL database. It connects to the
+    'postgres' database and checks if 'songstorage' exists and connects to it, if
+    it does not, it creates it, and creates the 'songs' table.
+
+    Handles any errors that may occur during interacting with the database.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database="postgres")
         conn.autocommit = True
@@ -52,15 +94,50 @@ def database_setup():
 
 
 def create_folder():
+    """
+    This method creates the folder 'Storage' if it does not exist.
+    If the folder already exists, it specifies it.
+
+    Handles any errors that may occur during folder creation.
+
+    Returns:
+        None
+    """
+
     folder_path = "Storage"
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-        print(f"Storage folder created.")
-    else:
-        print(f"Storage folder exists.")
+    try:
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            print("Storage folder created.")
+        else:
+            print("Storage folder exists.")
+    except OSError as e:
+        print(f"Error creating folder '{folder_path}': {e}")
 
 
 def add_song(song_path, artist, song_name, release_date, tags):
+    """
+    This method adds a song to the 'Storage' folder and its metadata to the database.
+    It validates the given path is a valid song with one of the supported extensions,
+    check the release date format, and the other args to not be NULL. Then, if these
+    requirements are met, it copies the song file to the 'Storage' folder and inserts
+    the metdata into the 'songs' table in the 'songstorage' database, returning the id.
+    It also logs success and error messages to the console.
+
+    Args:
+        song_path (str): The path of the file.
+        artist (str): The name of the artist.
+        song_name (str): The name of the song.
+        release_date (str): The release date as 'YYYY-MM-DD'.
+        tags (str list): A list of tags associated with the song.
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the adding process.
+
+    Returns:
+        None(it only prints the id of the song inserted into the 'songs' table)
+    """
+
     try:
         if not os.path.isfile(song_path):
             print("Error: The file does not exist or is a directory.")
@@ -107,6 +184,23 @@ def add_song(song_path, artist, song_name, release_date, tags):
 
 
 def delete_song(song_id):
+    """
+    This method deletes a song from both the 'Storage' folder and its metadata from
+    the database. It retrieves the file name from the 'Songs' table by using the id,
+    deletes the song with the specified path from the 'Storage' folder if it exists
+    and then removes the metadata from the database 'songstorage'.
+    It also logs success and error messages to the console.
+
+    Args:
+        song_id (int): The ID of the song to be deleted.
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the deletion process.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
         cursor = conn.cursor()
@@ -140,6 +234,23 @@ def delete_song(song_id):
 
 
 def modify_data(song_id):
+    """
+    This method modifies the metadata of a song in the database 'songstorage'.
+    It retrieves the current song data from the 'Songs' table by the song ID and
+    allows the user to insert new values or to keep the same values if Enter is
+    pressed. Then, it updates the song metadata in the database 'songstorage'.
+    It also logs success and error messages to the console.
+
+    Args:
+        song_id (int): The ID of the song to be modified.
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the modifying process.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
         cursor = conn.cursor()
@@ -180,6 +291,23 @@ def modify_data(song_id):
 
 
 def search(criteria):
+    """
+    This method searches for songs in the database 'songstorage' based on specified
+    criteria. It allows searching for songs that match the specified fields given by
+    key to the specified values, each criterion being comma-separated in the format
+    'key=value' and it supports case-insensitive search using 'ILIKE'.
+    It also logs success and error messages to the console.
+
+    Args:
+        criteria (str): Comma-separated criteria, e.g. "artist=Queen, tags=rap".
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the search process.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
         cursor = conn.cursor()
@@ -221,7 +349,9 @@ def search(criteria):
             print(f"Results found - {len(results)}:")
             for row in results:
                 print(
-                    f"ID: {row[0]}, File_name: {row[1]}, Artist: {row[2]}, Song_name: {row[3]}, Release_date: {row[4]}, Tags: {', '.join(row[5])}")
+                    f"ID: {row[0]}, File_name: {row[1]}, Artist: {row[2]},"
+                    f" Song_name: {row[3]}, Release_date: {row[4]}, Tags: {', '.join(row[5])}"
+                )
         else:
             print("Error: No song found.")
 
@@ -235,6 +365,24 @@ def search(criteria):
 
 
 def create_save_list(archive_path, criteria):
+    """
+    This method creates a ZIP archive containing songs that match the search criteria.
+    It retrieves songs based on the criteria and saves them to a ZIP archive created at
+    the specified path. The function supports the same searching style as the 'search'
+    method and the archive is created if at least one song is found.
+    It also logs success and error messages to the console.
+
+    Args:
+        archive_path (str): The path of the ZIP archive creation.
+        criteria (str): Comma-separated criteria, e.g. "artist=Queen, tags=rap".
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the archive creation process.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
         cursor = conn.cursor()
@@ -297,6 +445,23 @@ def create_save_list(archive_path, criteria):
 
 
 def play_song(song_id):
+    """
+    This method plays the song with the ID given by retrieving the file name from the
+    database 'songstorage' using `pygame`. It also allows the user to rewind or forward
+    the song by 10 seconds using left and right arrow keys and to stop the playback
+    using the ESC key.
+    It also logs success and error messages to the console.
+
+    Args:
+        song_id (int): The ID of the song to be played.
+
+    Handles any errors that may occur during interacting with the database and any other
+    error that occur during the song playback process.
+
+    Returns:
+        None
+    """
+
     try:
         conn = pg8000.connect(user=USER, password=PASSWORD, host=HOST, database=DB)
         cursor = conn.cursor()
@@ -364,6 +529,26 @@ def play_song(song_id):
 
 
 def main():
+    """
+    Main method for the SongStorage application.
+    This method sets up the database and storage folder, then provides an interactive loop
+    where the user can manage their music library by adding, deleting, modifying, searching,
+    playing songs and creating save lists by typing different commands.
+
+    Commands:
+        - help: Display the list of available commands.
+        - add_song: Add a song to the storage and its metadata to the database.
+        - delete_song: Delete a song and its metadata using its ID.
+        - modify_data: Modify metadata for a song using its ID.
+        - search: Search for songs based on criteria (e.g. "artist=Queen, tags=rap").
+        - create_save_list: Create an archive of songs based on specified criteria.
+        - play: Play a song using its ID.
+        - quit: Exit the application.
+
+    Returns:
+        None
+    """
+
     database_setup()
     create_folder()
 
@@ -417,6 +602,10 @@ def main():
 
 
 if __name__ == "__main__":
+    """
+    Script entry point for SongStorage.
+    It ensures the main function runs when the script is executed
+    """
     main()
 
 
